f3a4faf3acbc43eb82cbf4a88cedacf6fb13f253
JamesWu <jwu_nyc@yahoo.com>
2021-04-05 10:15:42 -0400

    completed

--- src/components/HomeSearch.js
+++ src/components/HomeSearch.js
@@ -4,6 +4,9 @@ import {Text, View, StyleSheet} from 'react-native';
 export default HomeSearch = props => {
   return (
     <View>
+      <View style={styles.inputBox}>
+        <Text style={styles.inputText}>Where to</Text>
+      </View>
       <Text>This is my search</Text>
     </View>
   );
