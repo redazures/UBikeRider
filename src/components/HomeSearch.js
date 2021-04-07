370c48a5ca0a7aa57d8a7385986dc385d3b213dd
JamesWu <jwu_nyc@yahoo.com>
2021-04-06 23:05:34 -0400

    updated

--- src/components/HomeSearch.js
+++ src/components/HomeSearch.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import {Text, View, StyleSheet} from 'react-native';
+import AntDesign from 'react-native-vector-icons/AntDesign'
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons'
+
+import colors from '../config/colors';
 
 export default HomeSearch = props => {
   return (
     <View>
       <View style={styles.inputBox}>
         <Text style={styles.inputText}>Where to</Text>
+        <View style={styles.timeContainer}>
+          <AntDesign name={'clockcircle'} size={16} color={colors.dark}/>
+          <Text>Now</Text>
+          <MaterialIcons name={'keyboard-arrow-down'} size ={16} color={colors.dark}/>
+        </View>
       </View>
       <Text>This is my search</Text>
       <Text>This is my search</Text>
